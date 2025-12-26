cf1573432a665cffd381f1577eea288cd27c34c8
Max <maxslotov@icloud.com>
2025-12-26 23:31:33 +0200

    Add missing SurfacePincodeMap type definitions

--- satellite/src/device-types/api.ts
+++ satellite/src/device-types/api.ts
@@ -1,6 +1,6 @@
 import type HID from 'node-hid'
 import type { PixelFormat } from '@julusian/image-rs'
-import type { SatelliteControlDefinition, SatelliteSurfaceLayout } from '../generated/SurfaceManifestSchema.js'
+import type { SatelliteSurfaceLayout } from '../generated/SurfaceManifestSchema.js'
 import type { GridSize } from '../surfaceProxy.js'
 
 export type HIDDevice = HID.Device
@@ -47,4 +47,31 @@ export interface DeviceRegisterOutputVariable {
 
 export interface ClientCapabilities {
 	supportsSurfaceManifest: boolean
+}
+
+export type SurfacePincodeMap = SurfacePincodeMapPageSingle | SurfacePincodeMapPageMultiple | SurfacePincodeMapCustom
+export interface SurfacePincodeMapCustom {
+	type: 'custom'
+}
+export interface SurfacePincodeMapPageSingle extends SurfacePincodeMapPageEntry {
+	type: 'single-page'
+	pincode: [number, number] | null
+}
+export interface SurfacePincodeMapPageMultiple {
+	type: 'multiple-page'
+	pincode: [number, number]
+	nextPage: [number, number]
+	pages: Partial<SurfacePincodeMapPageEntry>[]
+}
+export interface SurfacePincodeMapPageEntry {
+	0: [number, number]
+	1: [number, number]
+	2: [number, number]
+	3: [number, number]
+	4: [number, number]
+	5: [number, number]
+	6: [number, number]
+	7: [number, number]
+	8: [number, number]
+	9: [number, number]
 }
